f4513c09f84beceb416b45c40efcc70aaf868e04
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-11-07 10:50:54 -0700

    finished tic tac toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -21,58 +21,69 @@ class MyFrame1 ( wx.Frame ):
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 
+		self.matrix = [[None,None,None],[None,None,None],[None,None,None]]
 
 		gSizer2 = wx.GridSizer( 4, 3, 0, 0 )
 		
 		self.m_button1 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button1.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button1.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button1, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button2 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button2.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button2.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button3 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button3.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button3.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button3, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button4 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button4.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button4.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button4, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button5 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button5.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button5.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button5, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button6 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button6.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button6.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button6, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button7 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button7.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button7.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button7, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button8 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button8.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button8.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button8, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_button9 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,90 ), 0 )
 		self.m_button9.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BACKGROUND ) )
+		self.m_button9.SetFont( wx.Font( 28, 74, 90, 90, False, "Arial" ) )
 		
 		gSizer2.Add( self.m_button9, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
 		# bottom
 		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Winner: ", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText2.Wrap( -1 )
-		gSizer2.Add( self.m_staticText2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
+		self.m_staticText2.SetFont( wx.Font( 20, 74, 90, 90, False, "Arial" ) )
+		gSizer2.Add( self.m_staticText2, 0, wx.ALL|wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL, 5 )
 		
 		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText1.Wrap( -1 )
@@ -115,42 +126,79 @@ class MyFrame1 ( wx.Frame ):
 	# Virtual event handlers, overide them in your derived class
 	def Cross1( self, event ):
 		self.m_button1.SetLabel("X")
+		self.matrix[0][0] = 1
+		self.test()
 	def Knots1( self, event ):
 		self.m_button1.SetLabel("O")
+		self.matrix[0][0] = 0
+		self.test()
 	def Cross2( self, event ):
 		self.m_button2.SetLabel("X")
+		self.matrix[0][1] = 1
+		self.test()
 	def Knots2( self, event ):
 		self.m_button2.SetLabel("O")
+		self.matrix[0][1] = 0
+		self.test()
 	def Cross3( self, event ):
 		self.m_button3.SetLabel("X")
+		self.matrix[0][2] = 1
+		self.test()
 	def Knots3( self, event ):
 		self.m_button3.SetLabel("O")
+		self.matrix[0][2] = 0
+		self.test()
 	def Cross4( self, event ):
 		self.m_button4.SetLabel("X")
+		self.matrix[1][0] = 1
+		self.test()
 	def Knots4( self, event ):
 		self.m_button4.SetLabel("O")
+		self.matrix[1][0] = 0
+		self.test()
 	def Cross5( self, event ):
 		self.m_button5.SetLabel("X")
+		self.matrix[1][1] = 1
+		self.test()
 	def Knots5( self, event ):
 		self.m_button5.SetLabel("O")
+		self.matrix[1][1] = 0
+		self.test()
 	def Cross6( self, event ):
 		self.m_button6.SetLabel("X")
+		self.matrix[1][2] = 1
+		self.test()
 	def Knots6( self, event ):
 		self.m_button6.SetLabel("O")
+		self.matrix[1][2] = 0
+		self.test()
 	def Cross7( self, event ):
 		self.m_button7.SetLabel("X")
+		self.matrix[2][0] = 1
+		self.test()
 	def Knots7( self, event ):
 		self.m_button7.SetLabel("O")
+		self.matrix[2][0] = 0
+		self.test()
 	def Cross8( self, event ):
 		self.m_button8.SetLabel("X")
+		self.matrix[2][1] = 1
+		self.test()
 	def Knots8( self, event ):
 		self.m_button8.SetLabel("O")
+		self.matrix[2][1] = 0
+		self.test()
 	def Cross9( self, event ):
 		self.m_button9.SetLabel("X")
+		self.matrix[2][2] = 1
+		self.test()
 	def Knots9( self, event ):
 		self.m_button9.SetLabel("O")
+		self.matrix[2][2] = 0
+		self.test()
 
 	def reset(self, event):
+		self.matrix = [[None,None,None],[None,None,None],[None,None,None]]
 		self.m_button1.SetLabel("")
 		self.m_button2.SetLabel("")
 		self.m_button3.SetLabel("")
@@ -160,6 +208,46 @@ class MyFrame1 ( wx.Frame ):
 		self.m_button7.SetLabel("")
 		self.m_button8.SetLabel("")
 		self.m_button9.SetLabel("")
+		self.m_staticText2.SetLabel("Winner: ")
+
+	def test(self):
+
+		# horizontal
+
+
+		for i in range(3):
+			hor = []
+			hor.append(self.matrix[i][i])
+
+			col = []
+
+			# rows
+			if(len(set(self.matrix[i]))==1) and self.matrix[i][0] != None and self.matrix[i][1] != None and self.matrix[i][2] != None:
+				if self.matrix[i][i] == 1:
+					self.m_staticText2.SetLabel("Winner: X")
+				else: 
+					self.m_staticText2.SetLabel("Winner: O")
+			for j in range(3):
+
+				col.append(self.matrix[j][i])
+				if(len(set(col))==1) and self.matrix[0][j] != None and self.matrix[1][j] != None and self.matrix[2][j] != None:
+					if col[0] == 1:
+						self.m_staticText2.SetLabel("Winner: X")
+					else: 
+						self.m_staticText2.SetLabel("Winner: O")
+
+
+		if(len(set(hor))==1) and self.matrix[0][0] != None and self.matrix[1][1] != None and self.matrix[2][2] != None:
+			if self.matrix[0][0] == 1:
+				self.m_staticText2.SetLabel("Winner: X")
+			else: 
+				self.m_staticText2.SetLabel("Winner: O")
+
+		if(len(set([self.matrix[2][0], self.matrix[1][1], self.matrix[0][2]]))==1) and self.matrix[2][0] != None and self.matrix[1][1] != None and self.matrix[0][2] != None:
+			if self.matrix[2][0] == 1:
+				self.m_staticText2.SetLabel("Winner: X")
+			else: 
+				self.m_staticText2.SetLabel("Winner: O")
 
 	def Cal( self, event ):
 		event.Skip()
